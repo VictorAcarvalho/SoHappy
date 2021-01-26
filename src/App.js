d1aef331bdeb309fc6dd56d0b26b0d69d2503f06
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-01-26 12:58:02 -0300

    Refatorando o eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import Global from './style/global/global';
 
 function App() {
   return (
     <div className="App">
-      <Global>
-        <h1>Hello World</h1>
-      </Global>
+      <h1>Hello World</h1>
     </div>
   );
 }
