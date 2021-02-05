0bacbb5a8664c6a86144e6995eb3669763d18fea
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-02-05 11:40:46 -0300

    implementa a pagina notFound

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Global from '../../style/global/global';
 import { Container } from './style';
 import photoNotFound from '../../assets/undraw_empty_xct9.svg';
@@ -10,6 +12,9 @@ function NotFound() {
       <h1>404</h1>
       <h2>Página não encontrada</h2>
       <img src={photoNotFound} alt="Ilustration" />
+      <Link to="/">
+        <button>Voltar para a página inicial</button>
+      </Link>
     </Container>
   );
 }
--- src/pages/404/style.js
+++ src/pages/404/style.js
@@ -5,10 +5,10 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 5rem;
+  margin-top: 2rem;
 
   h1 {
-    font-size: 15rem;
+    font-size: 11rem;
   }
   h2 {
     font-size: 3rem;
@@ -16,5 +16,18 @@ export const Container = styled.div`
   img {
     width: 38rem;
     margin-top: 5rem;
+  }
+  button {
+    background-color: #919cf9;
+    margin-top: 2rem;
+    border-radius: 0.8rem;
+    padding: 1.2rem;
+    font-size: 1.8rem;
+    outline: none;
+    border: 0;
+    font-family: 'Nunito', cursive;
+  }
+  button:hover {
+    cursor: pointer;
   }
 `;
