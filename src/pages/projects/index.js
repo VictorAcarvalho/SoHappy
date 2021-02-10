9b3799f7151a4783111b65f05a73ca93c8ea036e
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-02-10 00:37:51 -0300

    implementa a animaçao

---
+++ src/pages/projects/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Global from '../../style/global/global';
+import { Container } from './style';
+
+function Projects() {
+  return (
+    <Container>
+      <div>
+        <h1>SoHappy</h1>
+        <h2>Localize projetos próximos a você</h2>
+      </div>
+      <Global />
+    </Container>
+  );
+}
+
+export default Projects;
