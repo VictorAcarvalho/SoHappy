f31bba6a11fc1c870e49e7a4707f8d5901f383d5
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-11-16 16:52:10 -0300

    retira o leafleet

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import Global from '../../style/global/global';
 import { Container } from './style';
 
@@ -11,15 +10,6 @@ function Projects() {
         <h2>Localize projetos próximos a você</h2>
       </div>
       <div className="divMap">
-        <MapContainer center={[51.505, -0.09]} zoom={10} scrollWheelZoom>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>Teste</Popup>
-          </Marker>
-        </MapContainer>
         <Global />
       </div>
     </Container>
