dccf0e5c5a8de7e536495350c65b4b14bf6a8395
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-11-17 22:50:16 -0300

    implementa o basemap e o mapProvider nos projects

---
+++ src/context/MapView/index.js
@@ -0,0 +1,23 @@
+import { createContext, useState, useContext } from 'react';
+
+const MapViewContext = createContext();
+
+export default function MapViewProvider({ children }) {
+  const [mapView, setMapView] = useState([]);
+
+  return (
+    <MapViewContext.Provider
+      value={{
+        mapView,
+        setMapView,
+      }}
+    >
+      {children}
+    </MapViewContext.Provider>
+  );
+}
+
+export function useMapView() {
+  const { mapView, setMapView } = useContext(MapViewContext);
+  return { mapView, setMapView };
+}
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import BaseMap from '../../components/Basemap/index'
 import Global from '../../style/global/global';
 import { Container } from './style';
+import MapviewProvider from '../../context/MapView/index';
 
 function Projects() {
+  const MapSettings = {
+    center: [-45.9, -23.4],
+    zoom: 8,
+    basemap: 'topo-vector',
+  };
+
   return (
     <Container>
       <div>
@@ -10,6 +18,11 @@ function Projects() {
         <h2>Localize projetos próximos a você</h2>
       </div>
       <div className="divMap">
+        <MapviewProvider>
+          <BaseMap
+            center={MapSettings.center}
+          />
+        </MapviewProvider>
         <Global />
       </div>
     </Container>
